ec27e9f3b4624362186e277ea1e452dc808b0c65
Laura Felce <crepu83@yahoo.it>
2019-05-28 09:00:18 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,2 +1,30 @@
 class SchoolClassesController < ApplicationController
-end  
+
+  def index
+		@school_classes = SchoolClass.all
+	end
+
+	def show
+		@school_class = SchoolClass.find(params[:id])
+	end
+
+	def new
+		@school_class = SchoolClass.new
+	end
+
+	def create
+	  @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+	  redirect_to student_path(@student)
+	end
+
+	def edit
+		@student = Student.find(params[:id])
+	end
+
+	def update
+	  @student = Student.find(params[:id])
+	  @student.update(params.require(:student).permit(:first_name, :last_name))
+	  redirect_to student_path(@student)
+	end
+
+end
