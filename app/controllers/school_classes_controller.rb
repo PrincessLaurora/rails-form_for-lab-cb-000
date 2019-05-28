a14cf500917f43162196db684ad775e4622f0c56
Laura Felce <crepu83@yahoo.it>
2019-05-28 09:02:14 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -22,9 +22,9 @@ class SchoolClassesController < ApplicationController
 	end
 
 	def update
-	  @student = Student.find(params[:id])
-	  @student.update(params.require(:student).permit(:first_name, :last_name))
-	  redirect_to student_path(@student)
+	  @school_class = SchoolClass.find(params[:id])
+	  @school_class.update(params.require(:school_class).permit(:title, :room_number))
+	   redirect_to school_class_path(@school_class)
 	end
 
 end
