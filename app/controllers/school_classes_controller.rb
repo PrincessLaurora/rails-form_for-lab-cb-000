aee67a471cffb4347d212260bdcfe9449f10991a
Laura Felce <crepu83@yahoo.it>
2019-05-28 09:41:05 +0000

    Automatically backed up by Learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -13,7 +13,8 @@ class SchoolClassesController < ApplicationController
 	end
 
 	def create
-	  @school_class = SchoolClass.create(title: params[:title], room_number: params[:room_number])
+	  @school_class = SchoolClass.new(school_class_params(:title, :room_number))
+    @school_class.save
 	  redirect_to school_class_path(@school_class)
 	end
 
