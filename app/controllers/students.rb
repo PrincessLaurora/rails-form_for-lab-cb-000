0d15ddf7dedee068387d6abbfe46c8a26ebd7cb6
Laura Felce <crepu83@yahoo.it>
2019-05-28 08:42:51 +0000

    Automatically backed up by Learn

--- app/controllers/students.rb
+++ app/controllers/students.rb
@@ -1,2 +1,29 @@
 class StudentsController < ApplicationController
+
+  def index
+		@students = Students.all
+	end
+
+	def show
+		@student = Student.find(params[:id])
+	end
+
+	def new
+		@student = Student.new
+	end
+
+	def create
+	  @student = Student.create(first_name: params[:first_name], last_name: params[:last_name])
+	  redirect_to student_path(@student)
+	end
+
+	def edit
+		@post = Post.find(params[:id])
+	end
+
+	def update
+	  @post = Post.find(params[:id])
+	  @post.update(params.require(:post).permit(:title, :description))
+	  redirect_to post_path(@post)
+	end
 end
